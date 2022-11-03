4224a5c6e94ff21bc9ba8ce677012974a89dc5bb
VasaviDN <vasavi.vzf95@gmail.com>
2022-11-02 20:56:25 -0500

    added selector files

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var treesRouter = require('./routes/trees');
 var gridBuildRouter = require('./routes/gridBuild');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -25,6 +26,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/trees', treesRouter);
 app.use('/gridBuild', gridBuildRouter);
+app.use('/selector', selectorRouter);
+
 
 
 // catch 404 and forward to error handler
